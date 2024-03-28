af891f74292f1b25e74f6a872acf04e8f6d1daf8
Cece Potakey <cecepotakey24@gmail.com>
2024-03-28 05:28:56 -0400

    Add User logout backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -13,7 +13,7 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 
 router.post('/test', (req, res) => {
-  res.json({ requestBody: req.body });
+    res.json({ requestBody: req.body });
 });
 
 module.exports = router;
