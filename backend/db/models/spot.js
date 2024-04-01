e7ecc7ad70f0bc2a08dd94bb0f3f72c41af989b0
Cece Potakey <cecepotakey24@gmail.com>
2024-04-01 14:57:16 -0400

    Spots and SpotImages models and migrations created with associations

---
+++ backend/db/models/spot.js
@@ -0,0 +1,100 @@
+'use strict';
+const { Model, Validator } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Spot extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Spot.hasMany(
+        models.SpotImage,
+        {
+          foreignKey: 'spotId',
+          onDelete: 'CASCADE',
+          hooks: true
+        }
+      )
+    }
+  }
+  Spot.init({
+    ownerId: {
+      type: DataTypes.INTEGER,
+      references: { model: 'Users' },
+      onDelete: 'CASCADE' //when a user is deleted, all their created spots should be deleted
+    },
+    address: {
+      type: DataTypes.STRING,
+      allownull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    city: {
+      type: DataTypes.STRING,
+      allownull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    state: {
+      type: DataTypes.STRING,
+      allownull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    country: {
+      type: DataTypes.STRING,
+      allownull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    lat: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        min: -90,
+        max: 90,
+        notEmpty: true
+      }
+    },
+    lng: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        min: -180,
+        max: 180,
+        notEmpty: true
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      allownull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    description: {
+      type: DataTypes.STRING,
+      allownull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allownull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+  },
+
+    {
+      sequelize,
+      modelName: 'Spot',
+    });
+  return Spot;
+};
