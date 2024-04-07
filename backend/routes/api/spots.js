958c9133b1084264d12eb0f67470a3ef68e58399
Cece Potakey <cecepotakey24@gmail.com>
2024-04-07 02:58:27 -0400

    pagination debugging

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -99,6 +99,16 @@ router.get('/', async (req, res, next) => {
         pagination.limit = size;
         pagination.offset = size * (page - 1);
     }
+    else if (size <= 0 && page <= 0) {
+        err.errors = {
+            size: "Size must be greater than or equal to 1",
+            page: "Page must be greater than or equal to 1"
+        }
+        err.title = 'Query parameter validation errors'
+        err.status = 400
+        err.message = "Bad Request"
+        return next(err)
+    }
     else if (page <= 0) {
         const err = new Error
         err.errors = { page: "Page must be greater than or equal to 1" }
@@ -110,21 +120,13 @@ router.get('/', async (req, res, next) => {
     else if (size <= 0) {
         const err = new Error
         err.errors = { size: "Size must be greater than or equal to 1" }
+        err.errors = { page: "Page must be greater than or equal to 1" }
         err.title = 'Query parameter validation errors'
         err.status = 400
         err.message = "Bad Request"
         return next(err)
     }
-    else if (size <= 0 && page <= 0) {
-        err.errors = {
-            size: "Size must be greater than or equal to 1",
-            page: "Page must be greater than or equal to 1"
-        }
-        err.title = 'Query parameter validation errors'
-        err.status = 400
-        err.message = "Bad Request"
-        return next(err)
-    }
+
     // } else if (page <= 0) {
     //     const err = new Error
     //     err.status = 400
