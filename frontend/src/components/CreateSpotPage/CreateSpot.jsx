641b9cc44f24e7c77df8ed21b0fc2dc2c190534a
Cece Potakey <cecepotakey24@gmail.com>
2024-04-22 06:03:28 -0400

    working on reset feature

--- frontend/src/components/CreateSpotPage/CreateSpot.jsx
+++ frontend/src/components/CreateSpotPage/CreateSpot.jsx
@@ -65,11 +65,11 @@ const CreateSpot = () => {
             setName('')
             setDescription('')
             setPrice('')
-            // setPreviewImage('')
-            // setImage1('')
-            // setImage2('')
-            // setImage3('')
-            // setImage4('')
+            setPreviewImage('')
+            setImage1('')
+            setImage2('')
+            setImage3('')
+            setImage4('')
         }
 
         const newSpot = await dispatch(CreatNewSpot(Spotpayload)).catch(
@@ -104,7 +104,7 @@ const CreateSpot = () => {
 
             })
             reset()
-            
+
             if (!errors.length && !imageError.length) {
                  navigate(`/spots/${spotId}`)
             }
